170ab008ebaba8c224e9db89afb734a4b013d8f0
ManarBenHmida <139473066+ManarBenHmida@users.noreply.github.com>
2023-11-06 09:23:41 +0100

    Update Main.java

--- org/example/Main.java
+++ org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import proxy.CalculWS;
+import proxy.CalculWSService;
+public class ClientWS {
+public static void main(String[] args) {
+ CalculWS proxy = new CalculWSService().getCalculWSPort();
+ System.out.println(proxy.somme(4,5));
     }
 }
